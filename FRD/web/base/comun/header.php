a65d10f4683df192c66bfb68eeb2de6f11d04a06
Arturo Zambrano <turoar2006@gmail.com>
2019-11-01 23:42:04 +0100

    FRD

---
+++ FRD/web/base/comun/header.php
@@ -0,0 +1,16 @@
+<header>
+    <div class = "logo">
+        <img src="img/logo.png" />
+    </div>
+    <div class = "nav">
+        <a class = "nav-link" href="">HOME</a><span class="dot"></span>
+        <a class = "nav-link" href="">PROYECTOS</a><span class="dot"></span>
+        <a class = "nav-link" href="">SERVICIOS</a><span class="dot"></span>
+        <a class = "nav-link" href="">FRD</a><span class="dot"></span>
+        <a class = "nav-link" href="">CONTACTO</a>
+        
+    </div>
+    <div class = "footer">
+
+    </div>
+</header>
---
+++ FRD/web/base/index.php
@@ -0,0 +1,32 @@
+<!DOCTYPE html>
+<html lang="es">
+<head>
+  <meta charset="utf-8">
+  <title>FRD</title>
+  <meta name="description" content="">
+  <meta name="author" content="Arturo J. Zambrano P.">
+
+  <!-- Mobile Specific Metas -->
+  <meta name="viewport" content="width=device-width, initial-scale=1">
+
+  <!-- CSS -->
+  <link rel="stylesheet" href="css/style.css">
+  <link href="/assets/css/all.css" rel="stylesheet"> <!--load all styles -->
+
+  <link rel="stylesheet" media="(max-width:800px)" href="css/style-small.css">
+  <link rel="stylesheet" media="(min-width:801px)" href="css/style-large.css">
+
+  <!-- Favicon -->
+  <link rel="icon" type="image/png" href="img/favicon.png">
+
+</head>
+<body>
+  <?php include("comun/header.php"); ?>
+  <main>
+    
+  </main>
+<!-- End Document
+  –––––––––––––––––––––––––––––––––––––––––––––––––– -->
+</body>
+
+</html>
